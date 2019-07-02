d5fcc709083c346c98de462d20983eacef157665
Drew Shotwell <drewshotwell4@gmail.com>
2019-07-02 16:15:19 -0500

    f

--- lab2/Jacks.js
+++ lab2/Jacks.js
@@ -8,13 +8,13 @@ class Jack extends CompoundModel {
         const yTrans = mat4.create();
         const zTrans = mat4.create();
 
-        /*mat4.scale(xTrans, xTrans, [1, 0.1, 0.1]);
+        mat4.scale(xTrans, xTrans, [1, 0.1, 0.1]);
         
         mat4.rotateZ(yTrans, yTrans, Math.PI / 2);
         mat4.scale(yTrans, yTrans, [1, 0.1, 0.1]);
         
         mat4.rotateY(zTrans, zTrans, Math.PI / 2);
-        mat4.scale(zTrans, zTrans, [1, 0.1, 0.1]);*/
+        mat4.scale(zTrans, zTrans, [1, 0.1, 0.1]);
 
         super.addChild(bar, xTrans);
         super.addChild(bar, yTrans);
