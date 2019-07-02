5ebb66b1bb6c179e92f2d1c75f5161626eaec667
Drew Shotwell <drewshotwell4@gmail.com>
2019-07-02 14:46:53 -0500

    c

---
+++ lab2/CompoundModel.js
@@ -0,0 +1,30 @@
+class CompoundModel {
+    constructor(){
+        this.collection = [];
+    }
+
+    addChild(child, transform){
+        this.collection.push({child, transform});
+    }
+
+    remove(leaf){
+        for (let i = 0; i < this.collection.length; i++){
+            if (this.collection[i] === leaf) {
+                this.collection.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    getChild(key){
+        return this.collection[key];
+    }
+
+    render(gl, prgInfo, transform){
+        for (let leaf of this.collection){
+            leaf.child.render(gl, prgInfo, 
+                mat4.multiply(leaf.transform, leaf.transform, transform));
+        }
+    }
+}
--- lab2/Jacks.js
+++ lab2/Jacks.js
@@ -12,11 +12,11 @@ class Jack extends CompoundModel {
         
         mat4.scale(xTrans, xTrans, [1, 0.1, 0.1]);
         
-        mat4.scale(yTrans, yTrans, [1, 0.1, 0.1]);
         mat4.rotateZ(yTrans, yTrans, Math.PI / 2);
+        mat4.scale(yTrans, yTrans, [1, 0.1, 0.1]);
         
-        mat4.scale(zTrans, zTrans, [1, 0.1, 0.1]);
         mat4.rotateY(zTrans, zTrans, Math.PI / 2);
+        mat4.scale(zTrans, zTrans, [1, 0.1, 0.1]);
 
         /*const xTrans = mat4.create();
         const yTrans = mat4.create();
